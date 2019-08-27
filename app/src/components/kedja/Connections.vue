eb1d868379cd1006a992b2e622e6192d0061f677
Martin Törnros <martin.tornros@ri.se>
2019-08-27 13:50:57 +0200

    Reset user state on app load

--- app/src/components/kedja/Connections.vue
+++ app/src/components/kedja/Connections.vue
@@ -125,11 +125,11 @@ export default {
           inSelectedChainRight = c0.states.selected || c0.states.selectingConnected || c0.states.connectingConnected
         }
 
-        // Start point
+        // Left point
         let x0 = elLeft.getBoundingClientRect().x + elLeft.getBoundingClientRect().width - this.bounds.x + this.scroll.left
         let y0 = elLeft.getBoundingClientRect().y + elLeft.getBoundingClientRect().height / 2 - this.bounds.y
 
-        // End point
+        // Right point
         let x1 = elRight.getBoundingClientRect().x - this.bounds.x + this.scroll.left
         let y1 = elRight.getBoundingClientRect().y + elRight.getBoundingClientRect().height / 2 - this.bounds.y
 
@@ -151,13 +151,13 @@ export default {
         ctx.stroke()
 
         // Connection points
-        // Start circle
+        // Left circle
         ctx.fillStyle = this.userState.name === 'default' || inSelectedChainLeft ? '#ffdc27' : '#ffdc2722'
         ctx.beginPath()
         ctx.arc(x0, y0, inSelectedChainLeft ? 15 : 10, -Math.PI / 2, Math.PI / 2)
         ctx.fill()
 
-        // End circle
+        // Right circle
         ctx.fillStyle = this.userState.name === 'default' || inSelectedChainRight ? '#ffdc27' : 'ffdc2722'
         ctx.beginPath()
         ctx.arc(x1, y1, inSelectedChainRight ? 15 : 10, Math.PI / 2, -Math.PI / 2)
