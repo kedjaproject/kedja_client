4561511e51cf263787fd154eb39a435496ceb7c1
Robin Harms Oredsson <robin@betahaus.net>
2019-10-10 10:24:00 +0200

    Readded, it should be used

--- app/src/main.js
+++ app/src/main.js
@@ -4,13 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import { store } from '@/store'
-// import TransitionEndPlugin from 'transitionend-plugin-vue'
-// import VueDraggable from 'vue-draggable'
-// import 'vue-draggable/polyfills'
+import TransitionEndPlugin from 'transitionend-plugin-vue'
+import VueDraggable from 'vue-draggable'
+import 'vue-draggable/polyfills'
 
-// Vue.use(VueDraggable)
+Vue.use(VueDraggable)
 // Activate if using transitions for cards.
-// Vue.use(TransitionEndPlugin)
+Vue.use(TransitionEndPlugin)
 
 Vue.config.productionTip = false
 
